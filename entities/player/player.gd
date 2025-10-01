6ad79eb5e801d1e6a73a69e60d5950cfbbd5d73d
xconter <mihiriyer2008@gmail.com>
2025-10-01 10:22:09 -0400

    hi

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,7 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @onready  var player_input_synchronizer_component : PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
-@onready var weapon_root: Node2D = $WeaponRoot
+@onready var weapon_root: Node2D = $Visuals/WeaponRoot
 @onready var health_component: HealthComponent = $HealthComponent
 
 var bullet_scene : PackedScene = preload("uid://ds1e0jqofdabu")
@@ -15,15 +15,16 @@ func _ready():
 	health_component.died.connect(_on_died)
 	
 func _process(_delta: float) -> void:
-	var aim_position = weapon_root.global_position \
-		+ player_input_synchronizer_component.aim_vector
-	weapon_root.look_at(aim_position)
-
 	if is_multiplayer_authority():
 		velocity = player_input_synchronizer_component.movement_vector * 100
 		move_and_slide()
 		if player_input_synchronizer_component.is_attack_pressed:
 			try_create_bullet()
+
+func update_aim_position():
+	var aim_position = weapon_root.global_position \
+		+ player_input_synchronizer_component.aim_vector
+	weapon_root.look_at(aim_position)
 
 func try_create_bullet():
 	if !fire_rate_timer.is_stopped():
